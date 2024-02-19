735810224ff15855bfdc461b3934257ce3386b59
Muzaffar Khozhimatov <xjmuzaffar@gmail.com>
2024-02-19 22:41:44 +0900

    Homepage and Update&Deleting Images done

--- src/components/shared/TransformedImage.tsx
+++ src/components/shared/TransformedImage.tsx
@@ -1,5 +1,7 @@
-import { dataUrl, debounce, getImageSize } from "@/lib/utils";
-import { CldImage } from "next-cloudinary";
+"use client";
+
+import { dataUrl, debounce, download, getImageSize } from "@/lib/utils";
+import { CldImage, getCldImageUrl } from "next-cloudinary";
 import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import React from "react";
@@ -13,7 +15,21 @@ const TransformedImage = ({
   setIsTransforming,
   hasDownload = false,
 }: TransformedImageProps) => {
-  const downloadHandler = () => {};
+  const downloadHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    download(
+      getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      }),
+      title
+    );
+  };
   return (
     <div className='flex flex-col gap-4'>
       <div className='flex-between'>
@@ -48,7 +64,7 @@ const TransformedImage = ({
             onError={() => {
               debounce(() => {
                 setIsTransforming && setIsTransforming(false);
-              }, 8000);
+              }, 8000)();
             }}
             {...transformationConfig}
           />
@@ -56,10 +72,11 @@ const TransformedImage = ({
             <div className='transforming-loader'>
               <Image
                 src='/assets/icons/spinner.svg'
-                alt='Transforming'
+                alt='Spinner'
                 width={50}
                 height={50}
               />
+              <p className='text-white/80'>Please wait...</p>
             </div>
           )}
         </div>
